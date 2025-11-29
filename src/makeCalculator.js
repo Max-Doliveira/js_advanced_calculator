a7f91dd84218ebe8c6ff8e4799df448d927293a7
Max-Vaio <max.pessoal@hotmail.com>
2025-11-28 23:46:46 -0300

    Arquivo_de_Solução

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,41 @@
  */
 function makeCalculator() {
   // write code here
-}
 
+  let result = 0;
+
+  const calculator = {
+    get result() {
+      return result;
+    },
+
+    add: (a, b) => a + b,
+
+    subtract: (a, b) => a - b,
+
+    multiply: (a, b) => a * b,
+
+    divide: (a, b) => {
+      if (b === 0) {
+        return NaN;
+      }
+
+      return a / b;
+    },
+
+    operate(callback, num) {
+      result = callback(result, num);
+
+      return this;
+    },
+
+    reset() {
+      result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
+}
 module.exports = makeCalculator;
